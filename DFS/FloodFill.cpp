7fbadcf4ce049398ed10d41416a9fdead4155699
sudhir pal <40147474+sudhir98pal@users.noreply.github.com>
2020-10-10 08:00:46 +0530

    Create FloodFill.cpp

---
+++ DFS/FloodFill.cpp
@@ -0,0 +1,71 @@
+/*An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+At the end, return the modified image.
+
+Example 1:
+Input: 
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: 
+From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+by a path of the same color as the starting pixel are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected
+to the starting pixel.
+Note:
+
+The length of image and image[0] will be in the range [1, 50].
+The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+*/
+class Solution {
+public:
+    int n;
+    int m;
+    int q;
+    bool isSafe(int x,int y)
+    {
+        if(x<0||y<0||x>=n||y>=m) return false;
+        return true;
+    }
+    void dfs(vector<vector<int>> &a,int x,int y,int p)
+    {
+        if(isSafe(x,y)==false) return ;
+        int dx[]={-1,0,1,0};
+        int dy[]={0,1,0,-1};
+a[x][y]=p;
+        for(int i=0;i<4;i++)
+        {
+            int nx=dx[i]+x;
+            int ny=dy[i]+y;
+            
+            if(isSafe(nx,ny)==true)
+            {
+                if(a[nx][ny]!=p&&a[nx][ny]==q)
+                {
+                    a[nx][ny]=p;
+                    dfs(a,nx,ny,p);
+                }
+            }
+                    
+                
+            
+        }
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& a, int x, int y, int p)
+    {
+             n=a.size();
+        if(n==0) return a;
+         m=a[0].size();
+      q=a[x][y];
+        dfs(a,x,y,p);
+    
+        
+        return a;
+    }
+};
