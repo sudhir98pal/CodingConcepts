1216ffcf6bb0f0271b63429485c76f2e9de68003
sudhir pal <40147474+sudhir98pal@users.noreply.github.com>
2020-10-14 17:56:31 +0530

    Update 001pair_in_array_whose_sum_is_closest_to_x.cpp

--- TwoPointers/001pair_in_array_whose_sum_is_closest_to_x.cpp
+++ TwoPointers/001pair_in_array_whose_sum_is_closest_to_x.cpp
@@ -1,1 +1,110 @@
 
+
+
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+#define ld long double
+#define vsort(v) sort(v.begin(),v.end())
+#define vrsort(v) sort(v.begin(),v.end(),greater<int>())
+#define mset(a,p) memset(a,p,sizeof(a))
+#define rep(i,n) for(int i=0;i<n;i++)
+#define all(x) x.begin(),x.end()
+#define pi 3.141592653589793238
+#define mode 1000000007
+#define put(c) cout<<c<<endl
+#define ulta(a) reverse(a.begin(),a.end())
+#define out(a) for(auto x:a) cout<<x<<" ";
+#define sp(arr) sort(arr,arr+sizeof(arr)/sizeof(arr[0]));
+#define bolt ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+class sudhir
+{
+public:
+
+    int * arr;
+    int target;
+    int n;
+    sudhir(int n,int target)
+    {
+
+        this->target=target;
+        this->n=n;
+        arr=new int[n];
+    }
+
+    void findClosestSumPair();
+
+};
+
+void sudhir::findClosestSumPair()
+{
+sp(arr) // sorting
+    int diff=10000;// max possible
+
+    int l=0;
+    int r=n-1;
+    int firstNumber;
+    int secondNumber;
+
+    while(l<r)
+    {
+        int tempDiff=abs(arr[l]+arr[r]-target);
+        if(tempDiff<diff)
+        {
+
+            firstNumber=arr[l];
+            secondNumber=arr[r];
+        }
+
+        if(arr[l]+arr[r]>target) r--;
+        else l++;
+
+
+    }
+
+
+
+cout<<firstNumber<<" "<<secondNumber<<endl;
+
+
+}
+
+int main()
+{
+     bolt
+     cout<<"Enter Number of test cases"<<endl;
+int t;
+cin>>t;
+
+while(t-->0)
+{
+    cout<<"Enter The value of N"<<endl;
+int n;
+cin>>n;
+
+int target;
+cout<<"Enter The value of target"<<endl;
+cin>>target;
+sudhir pal(n,target);
+
+
+
+rep(i,n)
+{
+
+    cin>>pal.arr[i];
+}
+
+pal.findClosestSumPair();
+
+
+
+}
+
+
+
+
+}
+
